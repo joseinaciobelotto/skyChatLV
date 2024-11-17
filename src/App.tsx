cfda16a42a0e8e85f8551ac85dd8cd482e91f82e
Jose Inácio Belotto <121182225+joseinaciobelotto@users.noreply.github.com>
2024-11-17 03:31:23 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ const App = () => {
         <Router>
             <div>
                 <Routes>
-                    <Route path="/" element={<Home />} />          
-                    <Route path="/login" element={<Login />} />
+                    <Route path="/home" element={<Home />} />          
+                    <Route path="/" element={<Login />} />
                     <Route path="/chat/:salaId" element={<Chat />} />
                 </Routes>
             </div>
